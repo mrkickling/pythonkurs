66a7f574c49e2ca938b38b099813282025ca0d7c
Joakim Loxdal <loxdal@kth.se>
2020-01-30 20:33:01 +0100

    Update course

---
+++ tillfalle1/listor.py
@@ -0,0 +1,8 @@
+greetings = ["God dag", "Hej"] # Lägger en lista i variabeln ‘lista’
+print(greetings) # printa listan igen
+
+greetings.remove("Hej") # tar bort “Hej” från listan
+print(greetings) # printa listan igen
+
+greetings.append("Tjenare") # lägger till ett element i listan
+print(greetings) # printa listan igen
