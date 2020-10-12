626d12846b351d8d5416936b5167e98d033bea0e
Joakim Loxdal <loxdal@kth.se>
2020-10-12 21:34:52 +0200

    Update uppg1-3.py

--- tillfalle4/losningar/uppg1-3.py
+++ tillfalle4/losningar/uppg1-3.py
@@ -2,7 +2,7 @@ import requests
 
 stad = input("Vilken stad vill du ha vädret från?: ")
 
-URL = "https://api.openweathermap.org/data/2.5/weather?q="+ stad +"&appid=831c45d6783bf7371e3237d852d5daf8"
+URL = "https://api.openweathermap.org/data/2.5/weather?q="+ stad +"&appid=--------hemligt---------"
 res = requests.get(URL)
 weather_dict = res.json()
 while weather_dict["cod"] == "404": # Om användaren skrev in en stad som inte finns
