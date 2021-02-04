4c6b8ade00e1b95a819c4270fd2d9aea4449bbd5
Joakim Loxdal <loxdal@kth.se>
2021-02-04 16:20:04 +0100

    Update uppg2.py

--- tillfalle2/losningar/uppg2.py
+++ tillfalle2/losningar/uppg2.py
@@ -1,4 +1,4 @@
 # Skriv ut alla jämna tal från 0 till och med 100
 
-for i in range(0, 1001, 2): # Vi går igenom alla tal från 0 tom 100 hoppar 2 steg i taget
+for i in range(0, 101, 2): # Vi går igenom alla tal från 0 tom 100 hoppar 2 steg i taget
     print(i)
